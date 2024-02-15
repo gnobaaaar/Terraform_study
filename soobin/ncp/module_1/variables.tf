2d640580565d5755c8b027e3b4458e5fb504b1f4
Song121099 <thdtnqls1210@gmail.com>
2024-02-15 09:54:01 +0900

    ncp-1

--- soobin/ncp/module_1/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  default = "KR"
-}
-variable "access_key" {
-    default = "BO5UggY14DcvqcPZ8bLM"
-}
-variable "secret_key" {
-    default = "XSoH2S2YLO6jr26GDTgsqV1xYBKDo1qOwKsqir75"
-}
